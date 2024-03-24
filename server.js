2dfeeb41c7bd7d09f8b0003a75ad1c1c831b69b2
ig <gaibullaeviz@gmail.com>
2024-03-24 18:55:56 +0700

    bugfix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 443;
+const port = 8080;
 
 const start = async () => {
     app.use(express.urlencoded({ extended: true }))
