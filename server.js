a4f6b69b8c2e49df590797301070f8f5cf7d25bf
ig <gaibullaeviz@gmail.com>
2024-03-24 18:30:16 +0700

    test 443 port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 443;
 
 const start = async () => {
     app.use(express.urlencoded({ extended: true }))
