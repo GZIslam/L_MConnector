a9554908e045cf2dfa267fc04bdaf1beac1e2604
ig <gaibullaeviz@gmail.com>
2024-03-24 19:10:23 +0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 443;
+const port = 5000;
 
 const start = async () => {
     app.use(express.urlencoded({ extended: true }))
