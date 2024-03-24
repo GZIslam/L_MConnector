4969eae546c0b7a6ca78e006dcd92ea3bddb0f0f
ig <gaibullaeviz@gmail.com>
2024-03-24 18:35:12 +0700

    bugfix

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 443;
+const port = 5000;
 
 const start = async () => {
     app.use(express.urlencoded({ extended: true }))
     app.use(express.json())
 
     app.post("/receipt", async (req, res) => {
-        const data = JSON.parse(req.body);
+        // const data = JSON.parse(req.body);
         console.log("receipt", req.body, data);
         res.send("health_check")
     })
