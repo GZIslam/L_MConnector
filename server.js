c706c4b35c03ea9a2b0b4e26caf14e05a5ebdc0f
Islam Gaibullaev <gaibullaeviz@gmail.com>
2024-03-27 16:45:06 +0700

    bugfix

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const request = require("./src/request");
 const products = (async () => await request("entity/product"))(); 
-const {getProducts, reqFields, PORT} = require("./src/misc");
+const {getProducts, requiredFields, PORT} = require("./src/misc");
 
 app.use(express.json());
 
 app.post("/receipt", async (req, res) => {
   const receipt = req.body;
-  const shipment = reqFields(receipt.payments ? receipt.payments[0].name : "Cash");
+  const shipment = requiredFields(receipt.payments ? receipt.payments[0].name : "Cash");
   shipment.positions = getProducts(receipt.line_items || [], products);
   console.log("shipment", JSON.stringify(shipment, null, 5))
   res.send({ status: 200 });
