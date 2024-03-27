e3644555da6a2a7694bed76ee0e8d00fcaa75462
Islam Gaibullaev <gaibullaeviz@gmail.com>
2024-03-27 14:16:16 +0700

    test

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const app = express();
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-const port = 443;
-
-const options = {
-    key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "localhost.pem")),
-};
-
-const server = https.createServer(options, app);
+const port = process.env.PORT;
 
 app.post("/receipt", async (req, res) => {
     console.log("receipt", req);
     res.send({status: 200})
 })
 
-server.listen(port, () => {
+app.get("/heartbeat", async (req, res) => {
+    console.log("receipt", req);
+    res.send("tuk tuk . . . tuk tuk")
+})
+
+
+app.listen(port, () => {
     console.log(`App listening on ${port} port`);
 });
