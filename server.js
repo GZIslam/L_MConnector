5a52533c2a45e735e6ff1479f4d3ad41953b8d41
Islam Gaibullaev <gaibullaeviz@gmail.com>
2024-03-27 17:14:40 +0700

    test

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const {getProducts, requiredFields, PORT} = require("./src/misc");
 app.use(express.json());
 
 app.post("/receipt", async (req, res) => {
-  if(!products) products = await request("entity/product"); 
+  if(!products) products = await request("entity/product");
   const receipts = req.body.receipts || [];
-  console.log("body", JSON.stringify(req.body, null, 5));
+  console.log("products", JSON.stringify(products, null, 5));
   console.log("receipts", JSON.stringify(receipts, null, 5));
   for(let i = 0; i < receipts.length; i++) {
     const receipt = receipts[i];
-    console.log("receipt", JSON.stringify(receipt, null, 5));
+    // console.log("receipt", JSON.stringify(receipt, null, 5));
     const shipment = requiredFields(receipt.payments ? receipt.payments[0].name : "Cash");
     const positions = getProducts(receipt.line_items || [], products);
-    console.log("positions", JSON.stringify(positions, null, 5));
+    // console.log("positions", JSON.stringify(positions, null, 5));
     shipment.positions = positions;
     console.log("shipment", JSON.stringify(shipment, null, 5));
     if(positions.length) {
