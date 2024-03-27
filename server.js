2013c35da1d4fd323a3b12442c9b4b9434e1f4c4
Islam Gaibullaev <gaibullaeviz@gmail.com>
2024-03-27 17:04:39 +0700

    bugfix

--- server.js
+++ server.js
@@ -8,18 +8,21 @@ app.use(express.json());
 
 app.post("/receipt", async (req, res) => {
   if(!products) products = await request("entity/product"); 
-  const receipt = req.body;
-  console.log("receipt", JSON.stringify(receipt, null, 5));
-  const shipment = requiredFields(receipt.payments ? receipt.payments[0].name : "Cash");
-  const positions = getProducts(receipt.line_items || [], products);
-  console.log("positions", JSON.stringify(positions, null, 5));
-  shipment.positions = positions;
-  console.log("shipment", JSON.stringify(shipment, null, 5));
-  if(positions.length) {
-    await request("entity/demand", {
-        method: "POST",
-        body: JSON.stringify(shipment)
-    })
+  const receipts = req.body.receipts || [];
+  for(let i = 0; i < receipts.length; i++) {
+    const receipt = receipts[i];
+    console.log("receipt", JSON.stringify(receipt, null, 5));
+    const shipment = requiredFields(receipt.payments ? receipt.payments[0].name : "Cash");
+    const positions = getProducts(receipt.line_items || [], products);
+    console.log("positions", JSON.stringify(positions, null, 5));
+    shipment.positions = positions;
+    console.log("shipment", JSON.stringify(shipment, null, 5));
+    if(positions.length) {
+        await request("entity/demand", {
+            method: "POST",
+            body: JSON.stringify(shipment)
+        })
+    }
   }
   res.send({ status: 200 });
 });
