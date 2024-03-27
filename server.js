770d35545b56ee7383083c7b5b9f0abfe0316d69
Islam Gaibullaev <gaibullaeviz@gmail.com>
2024-03-27 17:35:58 +0700

    bugfix test

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const app = express();
 const request = require("./src/request");
-let products = undefined;
 const {getProducts, requiredFields, PORT} = require("./src/misc");
+
+const app = express();
+let products = undefined;
 
 app.use(express.json());
 
 app.post("/receipt", async (req, res) => {
   if(!products) products = await request("entity/product");
   const receipts = req.body.receipts || [];
-//   console.log("products", JSON.stringify(products, null, 5));
-  console.log("receipts", JSON.stringify(receipts, null, 5));
+  console.log("body", JSON.stringify(req.body, null, 5));
+  //   console.log("products", JSON.stringify(products, null, 5));
+  //   console.log("receipts", JSON.stringify(receipts, null, 5));
   for(let i = 0; i < receipts.length; i++) {
     const receipt = receipts[i];
-    // console.log("receipt", JSON.stringify(receipt, null, 5));
     const shipment = requiredFields(receipt.payments ? receipt.payments[0].name : "Cash");
     const positions = getProducts(receipt.line_items || [], products);
-    // console.log("positions", JSON.stringify(positions, null, 5));
     shipment.positions = positions;
+    console.log("receipt", JSON.stringify(receipt, null, 5));
+    console.log("positions", JSON.stringify(positions, null, 5));
     console.log("shipment", JSON.stringify(shipment, null, 5));
     if(positions.length) {
         await request("entity/demand", {
